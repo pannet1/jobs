e737784cbc3b628286dd06c54cae292ad3012bb1
pannet1 <prog@ecomsense.in>
2021-03-03 13:54:44 +0530

    my second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# jobsite
+job site with job search, resume upload and login feature
+
+=== installation ===
+clone the repo to your webroot
+web folder is the public folder 
+from the root directory run composer to install the dependencies
---
+++ web/index.php
@@ -0,0 +1,31 @@
+<?php
+DEFINE("PROJECT_ROOT_DIR", __DIR__.'/../');
+
+/*
+ * The purpose of this file is to have one central file hold all the requirements
+ * to build up this app (index.php, cli or unit-test for example all share the same
+ * requirements) 
+ */
+$composer = PROJECT_ROOT_DIR.'vendor/autoload.php';
+$env = PROJECT_ROOT_DIR.'app/config/env.ini';
+$route = PROJECT_ROOT_DIR.'app/config/route.ini';
+
+require($composer);
+$f3 = Base::instance();
+$f3->DEBUG = 3;
+$f3->config($env,false);
+$f3->config($route, true);
+
+//$f3->config(PROJECT_ROOT_DIR.'app/config/cli_routes.ini', false);
+// In a production environment you want to disable logging on the database as this consumes memory
+// and can slow down any long running scripts you may have.
+if($f3->DEBUG === 0 && $f3->exists('DB')) {
+	$f3->DB->log(false);
+}
+
+// This file will be all the other plugins, services, tools that you need to put in the Fat-Free class
+require(PROJECT_ROOT_DIR.'app/config/services.php');
+
+//$f3->set('CACHE','memcache=localhost');
+//\Template::instance()->extend('pagebrowser', '\Pagination::renderTag');
+$f3->run();
